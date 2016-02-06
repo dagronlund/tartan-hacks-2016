b9340aa3fe3868ae5b873de74c61b11c3f1fb1ed
David Gronlund <dagronlund@gmail.com>
2016-02-06 12:25:59 -0500

    Fix server listener and add debuggging parameters.

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
-
-var http = require('http');
-var io = require('socket.io')(http);  
+var fs = require('fs');
+var http = require('http');  
 var port = process.env.PORT || 1337;
 
-http.createServer(function(req, res) {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello World\n');
-}).listen(port);
+var server = http.createServer(function(req, res) {
+	//console.log(req.url);
+	if (req.url == '/') {
+		res.writeHead(200, { 'Content-Type': 'text/html' });
+		res.end(fs.readFileSync('index.html'));
+	} else {
+		res.writeHead(200, { 'Content-Type': 'text/javascript' });
+		var file_contents = fs.readFileSync(__dirname + req.url);
+		console.log(req.url);
+		console.log(file_contents);
+		res.end(file_contents);
+	}
+});
+server.listen(port);
 
+var io = require('socket.io').listen(server);
 io.on('connection', function(socket){
-  console.log('a user connected');
-  socket.on('disconnect', function() {
-  	console.log('some one disconnected');
-  });
+	console.log('a user connected');
+	socket.on('disconnect', function() {
+		console.log('some one disconnected');
+	});
 });
